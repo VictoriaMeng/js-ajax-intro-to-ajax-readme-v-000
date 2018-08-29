308e325735ccb40b3db8c545142793295e2ec3ef
Victoria Meng <nights1ststar@protonmail.com>
2018-08-29 01:24:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function showRepositories(event, data) {
-  let repos = JSON.parse(this.responseText)
+  const repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
